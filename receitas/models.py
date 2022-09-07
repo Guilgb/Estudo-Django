ff98162201f3a0f48e16d8048f742a2f9e81c98a
guilherme <guilherme.gb@hotmail.com>
2022-09-07 19:28:35 -0300

    Refatoracão

--- receitas/models.py
+++ receitas/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import datetime
-
-from pessoas.models import Pessoa
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,10 +13,10 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     publicada = models.BooleanField(default=False)
     foto_receita = models.ImageField(
         upload_to='fotos/%d/%m/%Y', blank='True')
+    pessoa = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome_receita
