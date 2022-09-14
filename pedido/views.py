0519d91963876c821f4629a1f7739506f65f47f7
guilherme <guilherme.gb@hotmail.com>
2022-09-14 17:17:31 -0300

    quase pronto

--- pedido/views.py
+++ pedido/views.py
@@ -11,9 +11,18 @@ def pedidos(request):
 
 
 def pedidos_update(request):
-    receita_id = 1
-    receita_obj = Receita.objects.get(id=receita_id)
-
-    pedido_obj, new_obj = Pedidos.objects.new_or_get(request)
-    pedido_obj.receita.add(receita_obj)
-    return redirect(receita_obj.get_absolute_url())
+    print(request.POST)
+    receita_id = request.POST.get('receita_id')
+    if receita_id is not None:
+        try:
+            receita_obj = Receita.objects.get(id=receita_id)
+        except Receita.DoesNotExist:
+            print('Esse produto não esta mais em estoque')
+            return redirect("pedido:home_pedido")
+        pedido_obj, new_obj = Pedidos.objects.new_or_get(request)
+        if receita_obj in pedido_obj.produtos.all():
+            pedido_obj.produtos.remove(pedido_obj)
+        else:
+            pedido_obj.produtos.add(receita_obj)
+        request.session['pedido_items'] = pedido_obj.produtos.count()
+    return redirect("pedido")
